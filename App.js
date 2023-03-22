0ba6d68b03a47cbce32ce245ce995407737ac67c
kietdz <thelight3501@gmail.com>
2023-03-22 12:36:25 +0700

    commit

--- App.js
+++ App.js
@@ -7,9 +7,11 @@
  */
 
 import React from 'react';
-import { ImageBackground, StyleSheet, Text, View } from "react-native";
+import { StyleSheet } from "react-native";
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from "@react-navigation/stack";
+import { Provider } from "react-redux";
+import store from "./store";
 import Tabs from './navigation/tabs';
 import ChuyenTien from './screens/ChuyenTien';
 import TabsChuyenTien from './navigation/tabsChuyenTien';
@@ -21,26 +23,28 @@ import RutTienSoKhac from './screens/RutTienSoKhac';
 import QRCodeThanhToan from './screens/QRCodeThanhToan';
 const Stack = createStackNavigator();
 const App = () => (
-  <NavigationContainer styles={styles.container}>
-    <Stack.Navigator
-      screenOptions={{
-        headerShown: false
-      }}
-    >
+  <Provider store={store}>
+    <NavigationContainer styles={styles.container}>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false
+        }}
+      >
+        {/* Tabs */}
+        <Stack.Screen name="Home" component={Tabs} />
 
-      {/* Tabs */}
-      <Stack.Screen name="Home" component={Tabs} />
+        <Stack.Screen name="ChuyenTien" component={ChuyenTien} />
+        <Stack.Screen name="ChooseBank" component={ChooseBank} />
+        <Stack.Screen name="ChuyenTienStep1" component={TabsChuyenTien} />
+        <Stack.Screen name="NapRut" component={NapRut}></Stack.Screen>
+        <Stack.Screen name="NapTien" component={NapTien}></Stack.Screen>
+        <Stack.Screen name="RutTien" component={RutTien}></Stack.Screen>
+        <Stack.Screen name="RutTienSoKhac" component={RutTienSoKhac}></Stack.Screen>
+        <Stack.Screen name="QRCodeThanhToan" component={QRCodeThanhToan}></Stack.Screen>
+      </Stack.Navigator>
+    </NavigationContainer>
+  </Provider>
 
-      <Stack.Screen name="ChuyenTien" component={ChuyenTien} />
-      <Stack.Screen name="ChooseBank" component={ChooseBank} />
-      <Stack.Screen name="ChuyenTienStep1" component={TabsChuyenTien} />
-      <Stack.Screen name="NapRut" component={NapRut}></Stack.Screen>
-      <Stack.Screen name="NapTien" component={NapTien}></Stack.Screen>
-      <Stack.Screen name="RutTien" component={RutTien}></Stack.Screen>
-      <Stack.Screen name="RutTienSoKhac" component={RutTienSoKhac}></Stack.Screen>
-      <Stack.Screen name="QRCodeThanhToan" component={QRCodeThanhToan}></Stack.Screen>
-    </Stack.Navigator>
-  </NavigationContainer>
 );
 
 const styles = StyleSheet.create({
---
+++ services/auth.service.js
@@ -0,0 +1,38 @@
+import axios from "axios";
+
+const API_URL = "http://localhost:8080/api/auth/";
+
+const register = (username, email, password) => {
+  return axios.post(API_URL + "signup", {
+    username,
+    email,
+    password,
+  });
+};
+
+const login = (phone_number, password) => {
+  return axios
+    .post(API_URL + "signin", {
+      username,
+      password,
+    })
+    .then((response) => {
+      if (response.data.accessToken) {
+        
+      }
+
+      return response.data;
+    });
+};
+
+const logout = () => {
+  
+};
+
+const authService = {
+  register,
+  login,
+  logout,
+};
+
+export default authService;
